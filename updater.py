8dad5c97fd5eabd5b69d2c366e273ae7f771ec34
CaringEyeDev <caringeye@icloud.com>
2019-04-18 13:48:35 +0000

    updated updater

--- updater.py
+++ updater.py
@@ -1,4 +1,9 @@
 import os
 
-cmd = "sudo git --git-dir=/home/pi/the-eye/.git --work-tree=/home/pi/the-eye/ pull"
-cmd = "sudo rsync -r /home/pi/the-eye/ /var/www/html/"
+cmd_1 = "sudo git --git-dir=/home/pi/the-eye/.git --work-tree=/home/pi/the-eye/ pull"
+cmd_2 = "sudo rsync -r /home/pi/the-eye/ /var/www/html/"
+
+returned_value = os.system(cmd_1)
+print(returned_value)
+returned_value = os.system(cmd_2)
+print(returned_value)
